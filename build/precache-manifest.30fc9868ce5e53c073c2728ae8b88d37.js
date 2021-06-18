0c799849cbaa19a565b943b30461ea7d2ba431b9
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2021-06-18 22:28:02 +0530

    First build

---
+++ build/precache-manifest.30fc9868ce5e53c073c2728ae8b88d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf9e5cf3a56c8ebaa8758441a90fdcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0238b36f4accff19e08",
+    "url": "/static/css/main.25d5b1e7.chunk.css"
+  },
+  {
+    "revision": "9f37968ac26ec4b7fc1f",
+    "url": "/static/js/2.b404afb6.chunk.js"
+  },
+  {
+    "revision": "8184495294e4be92399d7a3744424f4a",
+    "url": "/static/js/2.b404afb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0238b36f4accff19e08",
+    "url": "/static/js/main.366a1f9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
